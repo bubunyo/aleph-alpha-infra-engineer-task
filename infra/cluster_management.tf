397a592100fcfed31381f366e48592f8e149683f
Bubunyo Nyavor <kidd.bubu@gmail.com>
2025-09-21 15:54:43 +0200

    move cluster resources around

--- infra/cluster_management.tf
+++ infra/cluster_management.tf
@@ -1,5 +1,100 @@
 # Kubernetes Namespaces for the guestbook application and observability stack
 
+resource "docker_container" "registry" {
+  name  = var.registry_name
+  image = "registry:2"
+
+  ports {
+    internal = 5000
+    external = var.registry_port
+    ip       = "127.0.0.1"
+  }
+
+  restart = "always"
+}
+
+resource "kind_cluster" "default" {
+  name           = "aa-cluster"
+  wait_for_ready = true
+
+  kind_config {
+    kind        = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+
+    containerd_config_patches = [
+      <<-EOP
+      [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${var.registry_port}"]
+        endpoint = ["http://${var.registry_name}:5000"]
+      EOP
+    ]
+
+    node {
+      role = "control-plane"
+
+      kubeadm_config_patches = [
+        <<-EOK
+        kind: InitConfiguration
+        nodeRegistration:
+          kubeletExtraArgs:
+            node-labels: "ingress-ready=true"
+        EOK
+      ]
+
+      extra_port_mappings {
+        container_port = 80
+        host_port      = 80
+        protocol       = "TCP"
+      }
+
+      extra_port_mappings {
+        container_port = 443
+        host_port      = 443
+        protocol       = "TCP"
+      }
+    }
+  }
+  depends_on = [docker_container.registry]
+}
+
+resource "null_resource" "connect_registry" {
+  depends_on = [kind_cluster.default]
+
+  provisioner "local-exec" {
+    command = "docker network connect kind ${var.registry_name} || true"
+  }
+}
+
+resource "kubernetes_config_map" "local_registry_hosting" {
+  depends_on = [kind_cluster.default]
+
+  metadata {
+    name      = "local-registry-hosting"
+    namespace = "kube-public"
+  }
+  data = {
+    "localRegistryHosting.v1" = <<-EOT
+    host: "localhost:${var.registry_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+    EOT
+  }
+}
+data "http" "ingress_nginx" {
+  url = "https://raw.githubusercontent.com/kubernetes/ingress-nginx/refs/heads/main/deploy/static/provider/kind/deploy.yaml"
+}
+
+resource "local_file" "ingress_nginx_yaml" {
+  content  = data.http.ingress_nginx.response_body
+  filename = "${path.module}/assets/ingress-nginx-deploy.yaml"
+}
+
+resource "null_resource" "apply_ingress_nginx" {
+  provisioner "local-exec" {
+    command = "kubectl apply -f ${local_file.ingress_nginx_yaml.filename}"
+  }
+
+  depends_on = [kind_cluster.default, local_file.ingress_nginx_yaml]
+}
+
 resource "kubernetes_namespace" "monitoring" {
   metadata {
     name = "monitoring"
--- infra/provider.tf
+++ infra/provider.tf
@@ -33,10 +33,6 @@ terraform {
       source  = "hashicorp/http"
       version = "3.5.0"
     }
-    grafana = {
-      source  = "grafana/grafana"
-      version = "~> 3.7"
-    }
   }
 }
 
@@ -47,63 +43,8 @@ data "local_file" "docker_sock" {
 provider "docker" {
   host = trimspace(data.local_file.docker_sock.content)
 }
+
 provider "kind" {}
-
-resource "docker_container" "registry" {
-  name  = var.registry_name
-  image = "registry:2"
-
-  ports {
-    internal = 5000
-    external = var.registry_port
-    ip       = "127.0.0.1"
-  }
-
-  restart = "always"
-}
-
-resource "kind_cluster" "default" {
-  name           = "aa-cluster"
-  wait_for_ready = true
-
-  kind_config {
-    kind        = "Cluster"
-    api_version = "kind.x-k8s.io/v1alpha4"
-
-    containerd_config_patches = [
-      <<-EOP
-      [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${var.registry_port}"]
-        endpoint = ["http://${var.registry_name}:5000"]
-      EOP
-    ]
-
-    node {
-      role = "control-plane"
-
-      kubeadm_config_patches = [
-        <<-EOK
-        kind: InitConfiguration
-        nodeRegistration:
-          kubeletExtraArgs:
-            node-labels: "ingress-ready=true"
-        EOK
-      ]
-
-      extra_port_mappings {
-        container_port = 80
-        host_port      = 80
-        protocol       = "TCP"
-      }
-
-      extra_port_mappings {
-        container_port = 443
-        host_port      = 443
-        protocol       = "TCP"
-      }
-    }
-  }
-  depends_on = [docker_container.registry]
-}
 
 # Configure providers to use Kind cluster
 provider "kubernetes" {
@@ -113,44 +54,6 @@ provider "kubernetes" {
   cluster_ca_certificate = kind_cluster.default.cluster_ca_certificate
 }
 
-resource "null_resource" "connect_registry" {
-  depends_on = [kind_cluster.default]
-
-  provisioner "local-exec" {
-    command = "docker network connect kind ${var.registry_name} || true"
-  }
-}
-
-resource "kubernetes_config_map" "local_registry_hosting" {
-  depends_on = [kind_cluster.default]
-
-  metadata {
-    name      = "local-registry-hosting"
-    namespace = "kube-public"
-  }
-  data = {
-    "localRegistryHosting.v1" = <<-EOT
-    host: "localhost:${var.registry_port}"
-    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
-    EOT
-  }
-}
-data "http" "ingress_nginx" {
-  url = "https://raw.githubusercontent.com/kubernetes/ingress-nginx/refs/heads/main/deploy/static/provider/kind/deploy.yaml"
-}
-
-resource "local_file" "ingress_nginx_yaml" {
-  content  = data.http.ingress_nginx.response_body
-  filename = "${path.module}/assets/ingress-nginx-deploy.yaml"
-}
-
-resource "null_resource" "apply_ingress_nginx" {
-  provisioner "local-exec" {
-    command = "kubectl apply -f ${local_file.ingress_nginx_yaml.filename}"
-  }
-
-  depends_on = [kind_cluster.default, local_file.ingress_nginx_yaml]
-}
 
 provider "helm" {
   kubernetes {
@@ -160,15 +63,3 @@ provider "helm" {
     cluster_ca_certificate = kind_cluster.default.cluster_ca_certificate
   }
 }
-
-# Grafana provider configuration
-provider "grafana" {
-  url = "http://grafana.localhost"
-  auth = "${var.grafana_admin_username}:${var.grafana_admin_password}"
-
-  #   Wait for Grafana to be ready before creating resources
-  retry_wait = 30
-}
-
-
-
